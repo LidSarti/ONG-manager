bc2fa49b489f98a0d993950b65dc57d7cc0f0af7
Camila Cunha <camilamachado22@hotmail.com>
2025-10-02 16:27:48 -0300

    Feat : Inclusão do secrets para ocultar credenciais de banco de dados e chave do Bucket em ambiente de desenvolvimento, como também variaveis de ambiente para produção no build do docker

--- Program.cs
+++ Program.cs
@@ -6,14 +6,28 @@ using Supabase;
 
 var builder = WebApplication.CreateBuilder(args);
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddUserSecrets<Program>();
+}
+
 builder.Services.AddControllersWithViews();
+
 
 builder.Services.AddDbContext<OngDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var supabaseUrl = builder.Configuration["Supabase:Url"];
+var supabaseKey = builder.Configuration["Supabase:Key"];
+
+if (string.IsNullOrEmpty(supabaseUrl) || string.IsNullOrEmpty(supabaseKey))
+{
+    throw new InvalidOperationException("Supabase configuration is missing. Please check your User Secrets or environment variables.");
+}
+
 builder.Services.AddScoped(provider =>
     new Client(
-        supabaseUrl: "https://maiqixucwgpeecwgpqxu.supabase.co",
-        supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haXFpeHVjd2dwZWVjd2dwcXh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM4NjMzMjUsImV4cCI6MjA1OTQzOTMyNX0.ZGvOBUMCcCL4mf0IdbmMPfAXORRJadGmu5ekbbGBuFQ",
+        supabaseUrl: supabaseUrl,
+        supabaseKey: supabaseKey,
         new SupabaseOptions
         {
             AutoConnectRealtime = true,
