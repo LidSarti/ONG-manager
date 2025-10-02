c9cffd8259ed59cf1e7a087eaf7d68e957d360d0
LidSarti <lid.sarti@gmail.com>
2025-10-01 22:21:11 -0300

    ♻️ refactor: inclusão de valor padrão para todos os campos

--- Models/CadastroAnimal.cs
+++ Models/CadastroAnimal.cs
@@ -7,7 +7,7 @@ public class CadastroAnimal
 {
     [Key]
     [Required]
-    public int id { get; set; }
+    public int id { get; set; } = 0;
     [Required]
     [StringLength(200)]
     public string nome { get; set; } = string.Empty;
@@ -15,22 +15,22 @@ public class CadastroAnimal
     [StringLength(200)]
     public string raca { get; set; } = string.Empty;
     [Required]
-    public int idade { get; set; }
+    public int idade { get; set; } = 0;
     [Required]
-    public int disponivel { get; set; }
+    public int disponivel { get; set; } = 0;
     public string? biografia { get; set; }
     [Required]
     public string cidade { get; set; } = string.Empty;
     [Required]
     public string estado { get; set; } = string.Empty;
     [Required]
-    public int tipo_animal { get; set; }
+    public int tipo_animal { get; set; } = 0;
     [ForeignKey("tipo_animal")]
     public TipoAnimal? TipoAnimal { get; set; }
     [Required]
-    public int porte_animal { get; set; }
+    public int porte_animal { get; set; } = 0;
     [ForeignKey("porte_animal")]
     public Porte? Porte { get; set; }
-    public virtual ICollection<Imagem>? Imagens { get; set; } = new List<Imagem>();
+    public virtual ICollection<Imagem>? Imagens { get; set; } = [];
 
 }
